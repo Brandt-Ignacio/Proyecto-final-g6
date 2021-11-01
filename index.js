24be179b04e1de1e41718f9ce0e41de12a415749
asd <asd>
2021-11-01 17:45:15 -0300

    Clase 1/11

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
-const PORT = 3000;
+const cors = require("cors")
+const PORT = 4000;
 const DB ='mongodb://localhost/notasapp'
 mongoose.connect(DB).then(() => console.log('DB conectada'));
 const app = express();
-
+app.use(corse.json());
 app.use(express.json());
 
 app.use('/',require("./api/note"));
