98c3295d2065223baabffa3f174f29689e9b4826
Brandt-Ignacio <81595011+Brandt-Ignacio@users.noreply.github.com>
2021-11-14 11:08:23 -0300

    Update note.js

--- api/note.js
+++ api/note.js
@@ -1,52 +1,42 @@
-const express = require ("express");
+// archivo /api/note.js
+const express = require('express');
 const router = express.Router();
-const Note = require("../models/Note");
+const Note = require('../models/Note');
 
 router
-.route("/api/notes")
-.get((req, res)=>{
-  Note.find((err,notes)=> {
-    res.json(notes);
-  });
-})
-.post((req, res)=>{
+  .route('/api/notes')
+  // buscar todas las notas
+  .get((req, res) => {
+    Note.find((err, notes) => {
+      res.json(notes);
+    })
+  })
+  // crear una nota usando los datos de req.body
+  .post((req, res) => {
     const note = new Note({
       title: req.body.title,
       text: req.body.text
     });
-    note.save((err,note)=> {
+    note.save((err, note) => {
       res.json(note);
     })
   });
 
 router
-.route("api/notes/:id")
-.get((req,res)=> {
-  Note.find({ id: req.params.id},(err,note) => {
-    res.json(note);
-  });
-})
-.put('/notes/:id', (req, res, next) => {
-  const note = {
-    title: req.body.title,
-    text: req.body.text,
-    updatedAt: Date.now()
-  };
-  const options = {
-    new: true,
-    omitUndefined: true
-  };
-  Note.findByIdAndUpdate(req.params.id, note, options).exec((err, note) => {
-    if (err) return next(err);
-    if (!note) return res.status(404).json({ msg: 'Not found' });
-    res.status(200).json(note);
-  });
-});
-
-.delete((req,res) => {
-  Note.findByIDAndRemove(req.params.id, (err)=> {
-    res.json({msg: 'nota borrada'});
+  .route('/api/notes/:id')
+  // buscar una nota por id
+  .get((req, res) => {
+    Note.find({ id: req.params.id }, (err, note) => {
+      res.json(note);
+    });
   })
-});
+  .put((req, res) => {
+    res.send('Actualizar nota');
+  })
+  .delete((req, res) => {
+    Note.findByIdAndRemove(req.params.id, (err) => {
+      res.json({ msg: 'Nota borrada' });
+    })
+  });
 
 module.exports = router;
