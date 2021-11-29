c94fe45d8df057b5ae3b5dc2ebed3e4bf38c06f2
Brandt-Ignacio <81595011+Brandt-Ignacio@users.noreply.github.com>
2021-11-29 17:00:18 -0300

    Update Nota.js

--- client/src/Nota.js
+++ client/src/Nota.js
@@ -2,17 +2,19 @@ const Nota = props => {
   const {title,text,id,deleteNote,updateNote} = props;
   return(
     <div className="nota">
+    <div className="title">
       <h2>{title}</h2>
+      </div>
       <p>{text}</p>
-      
-      <button onClick={() => deleteNote(id)}>Borrar</button>
-  
-      <button onClick={()=> updateNote(id)}>Editar</button>
+
+<div className="botones">
+<div className="borrar">
+      <button className="btn btn-danger" onClick={() => deleteNote(id)}>Borrar</button>
+</div>
+<div className="editar">
+      <button className="btn btn-success" onClick={()=> updateNote(id)}>Editar</button>
     </div>
+      </div>
+      </div>
   )
   ;
-
-
-};
-
-export default Nota;
